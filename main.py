ccf563f3c8e77f5f7ad375dec61ffbac4e0eb0c8
Mathis Poteau <mathis.poteau28@gmail.com>
2025-11-17 23:52:50 +0100

    ajout du tank T2 et des différents segment X4,X5, X6

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 from model import Cell, Sommet, Network, Simulation
 
 # Liste de sommets
-vertices = {"T1":["X1"], "X1":["X2"], "X2":["X3"], "X3":[]} # dictionnaire {nom_sommet: [voisins]}
-v_properties = {"T1":[100], "X1":[500, 70/3.6, 1000/3600, 20/3.6, 0], "X2":[500, 70/3.6, 1000/3600, 20/3.6, 0], "X3":[500, 70/3.6, 1000/3600, 20/3.6, 0]} # for the tanks the only parameter is x_0
+vertices = {"T1":["X1"], "T2":["X4"], "X1":["X2"], "X2":["X3"], "X3":["X4"],"X4":["X5"], "X5":["X6"],"X6":[]} # dictionnaire {nom_sommet: [voisins]}
+v_properties = {"T1":[100], "T2":[100], "X1":[500, 70/3.6, 1000/3600, 20/3.6, 0], "X2":[500, 70/3.6, 1000/3600, 20/3.6, 0], "X3":[500, 70/3.6, 1000/3600, 20/3.6, 0],\
+ "X4":[500, 70/3.6, 1000/3600, 20/3.6, 0], "X5":[500, 70/3.6, 1000/3600, 20/3.6, 0], "X6":[500, 70/3.6, 1000/3600, 20/3.6, 0]} # for the tanks the only parameter is x_0
 
 
 net = Network(vertices)
@@ -16,9 +17,13 @@ simu = Simulation(graph,sommets)
 # Définition des positions manuelles
 positions = {
     sommets["T1"]: (1, 1),
+    sommets["T2"]: (4, 1),
     sommets["X1"]: (1, 0),
     sommets["X2"]: (2, 0),
-    sommets["X3"]: (3, 0)
+    sommets["X3"]: (3, 0),
+    sommets["X4"]: (4, 0),
+    sommets["X5"]: (5, 0),
+    sommets["X6"]: (6, 0)
 }
 
 # print(graph.nodes)
